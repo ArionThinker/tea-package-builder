824e54d365ff610624b1937e97a1cec4c18875ff
Andrii Riabchenko <ryabchenko.andrew@gmail.com>
2023-08-20 19:44:19 +0300

    don't set icon for win

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const os = require('os');
 const path = require('path')
 
 function createWindow () {
@@ -42,5 +43,6 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-app.dock.setIcon(path.join(__dirname, 'images/logo.png'));
+if (os.platform() !== 'win32') {
+  app.dock.setIcon(path.join(__dirname, 'images/logo.png'));
+}
